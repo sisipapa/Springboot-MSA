d2d1f15c6d17d7aee0f3449ee2539f239725ca2c
ghkim <ghkim@enliple.com>
2021-08-26 15:06:22 +0900

    ip,port로 서비스 연결이 아닌 service명 연결!!

--- Gateway/src/main/java/com/sisipapa/study/msa/gateway/GatewayApplication.java
+++ Gateway/src/main/java/com/sisipapa/study/msa/gateway/GatewayApplication.java
@@ -2,14 +2,14 @@ package com.sisipapa.study.msa.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableDiscoveryClient
 @EnableZuulProxy
 @SpringBootApplication
 public class GatewayApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
     }
-
 }
--- Resource2/src/main/java/com/sisipapa/study/msa/resource2/Resource2Application.java
+++ Resource2/src/main/java/com/sisipapa/study/msa/resource2/Resource2Application.java
@@ -7,9 +7,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 @SpringBootApplication
 public class Resource2Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Resource2Application.class, args);
     }
-
 }
